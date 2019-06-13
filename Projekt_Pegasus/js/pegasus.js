9879f5a2c371c9fabe5b43b9434bb870f00b52af
voltae <ic17b096@technikum-wien.at>
2019-06-13 13:23:06 +0200

    index.html changed quest-field added div js-> added json parsing php-> added comupted index

--- Projekt_Pegasus/js/pegasus.js
+++ Projekt_Pegasus/js/pegasus.js
@@ -1,7 +1,8 @@
 window.addEventListener('load', setup);
 
 function setup() {
-    document.getElementById('next_quest').addEventListener('click', getNext);
+    document.getElementById('next-quest').addEventListener('click', getNext);
+
 }
 
 
@@ -10,14 +11,42 @@ function getNext() {
         xmlhttp =  new XMLHttpRequest();
     } 
     xmlhttp.onreadystatechange=function() {
-        if (xmlhttp.readyState == 4 && xmlhttp.status ==200) {
-            document.getElementById('quest_field').innerHTML=xmlhttp.responseText;
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            document.getElementById('quest-field').innerHTML=xmlhttp.responseText;
         }
     }
     xmlhttp.open('GET', '/../php/server.php');
-    xmlhttp.addEventListener('load', function(event) {
-        
-        console.log(XMLHttpRequest.responseText);
-    })
+    xmlhttp.addEventListener('load', getRespond);
+    xmlhttp.addEventListener('error', getError);
     xmlhttp.send();
+}
+
+function getRespond(event) {
+    var respond = JSON.parse(event.target.responseText);
+    var questionField = document.getElementById('question-field')
+    // add the question text
+    questionField.getElementsByTagName('h3')[0].innerText= respond.index + ". Frage"; // this is the hesader h3
+    questionField.getElementsByTagName('p')[0].innerText = respond.question; // this is the question paragraph
+
+    // add the answer buttons
+    var quest_field = document.getElementById('quest-field');
+    let alternatives = respond.alternatives;
+    alternatives.forEach(element => {
+        var button = document.createElement('button');
+        button.addEventListener('click', function() {
+            evaluateAnswer(element, respond.answer);
+        });
+
+        button.innerText = element;
+        quest_field.appendChild(button);
+    });
+}
+
+function evaluateAnswer(element, answer) {
+    console.log(element);
+    console.log(answer);
+}
+
+function getError() {
+
 }
--- Projekt_Pegasus/php/server.php
+++ Projekt_Pegasus/php/server.php
@@ -8,22 +8,22 @@ class Question {
     public $index;
     public $alternatives = [];
     
-  function __construct($question, $answer, $index, $alternatives) {
+  function __construct($question, $answer, $alternatives) {
       $this->question = $question;
       $this->answer = $answer;
-      $this->index= $index;
       $this->alternatives=$alternatives;
   }
 }
 $questions = array(
-     new Question("1. Frage", "1. richtige Antwort", 1, array('1.1 alternative', '1.2 alternative', '1.3 alternative')),
-     new Question("2. Frage", "2. richtige Antwort", 2, array('2.1 alternative', '2.2 alternative', '2.3 alternative')),
-     new Question("3. Frage", "3. richtige Antwort", 3, array('3.1 alternative', '3.2 alternative', '3.3 alternative')),
-     new Question("4. Frage", "4. richtige Antwort", 4, array('4.1 alternative', '4.2 alternative', '4.3 alternative')),
-     new Question("5. Frage", "5. richtige Antwort", 5, array('5.1 alternative', '5.2 alternative', '5.3 alternative'))
+     new Question("1. Frage", "1. richtige Antwort", array('1.1 alternative', '1.2 alternative', '1.3 alternative')),
+     new Question("2. Frage", "2. richtige Antwort", array('2.1 alternative', '2.2 alternative', '2.3 alternative')),
+     new Question("3. Frage", "3. richtige Antwort", array('3.1 alternative', '3.2 alternative', '3.3 alternative')),
+     new Question("4. Frage", "4. richtige Antwort", array('4.1 alternative', '4.2 alternative', '4.3 alternative')),
+     new Question("5. Frage", "5. richtige Antwort", array('5.1 alternative', '5.2 alternative', '5.3 alternative'))
   );
-
-$question = $questions[array_rand($questions)];
+$index = rand(0, count($questions) -1);
+$question = $questions[$index];
+$question->index = $index +1;
 
 $jsonQuest = json_encode($question);
 echo $jsonQuest;
